141586b1b9e25ebc240ef9c963e49ca6b69a39be
maurofallico <maurofallico@gmail.com>
2024-01-14 17:49:18 -0300

    testing

--- app/favorites/page.jsx
+++ app/favorites/page.jsx
@@ -18,6 +18,7 @@ export default function Favorites() {
         id,
         fav: false,
       });
+      paginarDatos()
     } catch (error) {
       console.error("Error removing fav:", error);
     }
