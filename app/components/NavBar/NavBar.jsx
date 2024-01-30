d8bf8008cf61b050d54d0a3b8d3beca7e4c0a667
maurofallico <maurofallico@gmail.com>
2024-01-30 11:30:17 -0300

    noAbout

--- app/components/NavBar/NavBar.jsx
+++ app/components/NavBar/NavBar.jsx
@@ -7,7 +7,6 @@ export default function NavBar (){
         <div className="bg-cyan-600 h-16 flex justify-center items-center gap-32">
             <NavLink href="/" className='bg-white rounded-md px-2 py-1 hover:bg-gray-200'>Home</NavLink>
             <NavLink href="/favorites" className='bg-white rounded-md px-2 py-1 hover:bg-gray-200'>Favorites</NavLink>
-            <NavLink href="/about" className='bg-white rounded-md px-2 py-1 hover:bg-gray-200'>About</NavLink>
         </div>
         </>
     )
