597b6cde241830b9e161e1e3b7ec093f394a13e7
maurofallico <maurofallico@gmail.com>
2024-02-06 11:13:24 -0300

    responsiveFixed

--- app/components/Detail/Detail.jsx
+++ app/components/Detail/Detail.jsx
@@ -42,7 +42,7 @@ export default function Detail({ setIsOpen, cardId }) {
   return (
     <>
         <div className="fixed inset-0 bg-black bg-opacity-30 backdrop-blur-sm flex justify-center items-center">
-          <div className="justify-center sm:h-fit pb-8 sm:w-fit min-h-[150px] min-w-[450px] max-w-[600px] bg-blue-50 shadow-2xl border-2 border-slate-700 p-3 sm:rounded-3xl flex flex-col items-center gap-2 w-screen">
+          <div className="justify-center sm:h-fit pb-8 sm:w-fit min-h-[150px] min-w-[450px] max-w-[600px] bg-blue-50 shadow-2xl border-2 border-slate-700 p-3 sm:rounded-3xl flex flex-col items-center gap-2 w-screen text-black">
             <button onClick={() => setIsOpen(false)} className="flex self-end sm:px-0 px-6">
               <CgClose className="hover:text-red-600 text-xl " />
             </button>
